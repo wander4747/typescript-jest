f56411195ac47fafd2adaab016ce7f92f93151ca
Wander <wander.bicalho@feedz.com.br>
2025-05-02 13:31:21 -0300

    create class cart

---
+++ src/cart/cart.ts
@@ -0,0 +1,36 @@
+import Product from "@/product/product";
+
+export default class Cart { 
+    private readonly _items: Product[] = []
+
+    addItem(product: Product): void {
+        this._items.push(product);
+    }
+
+    removeItem(item: Product): void {
+        const index = this._items.indexOf(item)
+        this._items.splice(index, 1)
+    }
+
+    get items(): readonly Product[] {
+        return this._items
+    }
+
+    total(): number {
+        let total = 0
+        
+        this._items.map(product => {
+            total += product.price
+        })
+        
+        return total
+    }
+
+    isEmpty(): boolean {
+        return this._items.length === 0
+    }
+    
+    clear(): void {
+        this._items.length = 0
+    }
+}
